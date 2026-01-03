996fc6b0d3b8349e63adc9c5d433beb154d227a8
AK <aamaal00777@gmail.com>
2026-01-03 17:25:36 +0400

    created models for database

---
+++ backend/src/models/Board.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const boardSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const Board = mongoose.model("Board", boardSchema);
+export default Board;
--- backend/src/server.js
+++ backend/src/server.js
@@ -5,13 +5,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
-
+const port = process.env.PORT;
 connectDB();
 
 app.use("/actions/boards", boardsRoutes);
 
-app.listen(5001, () => {
-  console.log("Server started on port 5001");
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
 
 //
